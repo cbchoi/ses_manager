1126001d3e592b88f48301c8870d9e75b126748d
Changbeom Choi <cbchoi@handong.edu>
2019-02-11 03:26:45 +0900

    Entity Structure Manager  - Inital Commit
     SES Manager
      - Inital Commit

---
+++ venv/src/ESManager.py
@@ -0,0 +1,40 @@
+import sys
+from Entity import *
+from Attribute import *
+from Definition import *
+import json
+from collections import OrderedDict
+
+class EntityManager(object):
+    def __init__(self):
+        self.entity_path = ""
+        self.root_entity = None
+
+    def set_entity_db_path(self, path):
+        self.entity_path = path
+
+    def retrive_entity_db_path(self):
+        return self.entity_path
+
+    def create_empty_entity_structure(self, type):
+        enum = AttributeType.resolve_type_from_str(type)
+        if enum == AttributeType.BEHAVIOR:
+            return BehavioralEntity("")
+        elif enum == AttributeType.STRUCTURAL:
+            return StructuralEntity("")
+        else:
+            return Entity("")
+
+    def create_system(self, entity:Entity):
+        self.root_entity = entity
+
+    def export_system_entity_structure(self, path="."):
+        entity_data = OrderedDict()
+        entity_data["name"] = self.root_entity.get_name()
+        entity_data["attributes"] = self.root_entity.attribute_to_list()
+
+        print(json.dumps(entity_data, ensure_ascii=False, indent="\t"))
+        pass
+
+    def import_system_entity_structure(self, path="."):
+        pass
---
+++ venv/src/ses_manager.py
@@ -0,0 +1,19 @@
+from ESManager import *
+
+esm = EntityManager()
+entity = esm.create_empty_entity_structure("STRUCTURAL")
+entity.set_name("System")
+entity.insert_input_port("in1")
+entity.insert_input_port("in2")
+
+entity.insert_output_port("out1")
+entity.insert_output_port("out2")
+
+entity.insert_entity("en")
+entity.insert_entity("en1")
+entity.insert_coupling(("", "in1"), ("en", "in"))
+entity.insert_coupling(("en", "out"), ("en1", "in"))
+entity.insert_coupling(("en1", "out"), ("", "out"))
+
+esm.create_system(entity)
+esm.export_system_entity_structure()
